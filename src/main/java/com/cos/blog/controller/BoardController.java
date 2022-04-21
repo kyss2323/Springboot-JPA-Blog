e9a6e4c85dd3a18a1b1251ae96d84c45d2f92e80
unknown <tmdflwndtn@gmail.com>
2022-04-21 19:58:15 +0900

    글목록 페이징 구현

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,15 +1,32 @@
 package com.cos.blog.controller;
 
 import com.cos.blog.configuration.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    BoardService boardService;
+
+
     @GetMapping({"","/"})
-    public String index(){
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
+        model.addAttribute("boards", boardService.글목록(pageable));
+        int startPage = ((pageable.getPageNumber()-1) / 10) * 10 + 1;
+        pageable.getPageSize();
+        int endPage = startPage + 10 - 1  > pageable.getPageSize() ? pageable.getPageSize() : startPage + 10 - 1;
+        model.addAttribute("startPageNo", startPage);
+        model.addAttribute("endPageNo", endPage);
+
         return "index";
     }
 
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -4,8 +4,12 @@ import com.cos.blog.model.Board;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class BoardService {
@@ -17,5 +21,9 @@ public class BoardService {
         board.setCount(0);
         board.setUser(user);
         boardRepository.save(board);
+    }
+
+    public Page<Board> 글목록(Pageable pageable){
+        return boardRepository.findAll(pageable);
     }
 }
